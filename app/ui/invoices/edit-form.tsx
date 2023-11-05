725dd3d2dd71311bfdda3788562f0894b57a1335
SemmyK <66922979+SemmyK@users.noreply.github.com>
2023-11-05 18:22:12 +0100

    added form validation, authentication and metadata

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { useFormState } from 'react-dom'
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
 import {
 	CheckIcon,
@@ -15,13 +15,18 @@ export default function EditInvoiceForm({
 	invoice,
 	customers,
 }: {
-	invoice: InvoiceForm | undefined
+	invoice: InvoiceForm
 	customers: CustomerField[]
 }) {
-	const updateInvoiceWithId = invoice && updateInvoice.bind(null, invoice.id)
+	const initialState = {
+		message: null,
+		errors: {},
+	}
 
+	const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+	const [state, dispatch] = useFormState(updateInvoiceWithId, initialState)
 	return (
-		<form action={updateInvoiceWithId}>
+		<form action={dispatch}>
 			<div className='rounded-md bg-gray-50 p-4 md:p-6'>
 				{/* Customer Name */}
 				<div className='mb-4'>
@@ -32,6 +37,7 @@ export default function EditInvoiceForm({
 						<select
 							id='customer'
 							name='customerId'
+							aria-describedby='customer-error'
 							className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
 							defaultValue={invoice && invoice.customer_id}
 						>
@@ -46,8 +52,18 @@ export default function EditInvoiceForm({
 						</select>
 						<UserCircleIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500' />
 					</div>
+					{state.errors?.customerId ? (
+						<div
+							id='customer-error'
+							aria-live='polite'
+							className='mt-2 text-sm text-red-500'
+						>
+							{state.errors.customerId.map((error: string) => (
+								<p key={error}>{error}</p>
+							))}
+						</div>
+					) : null}
 				</div>
-
 				{/* Invoice Amount */}
 				<div className='mb-4'>
 					<label htmlFor='amount' className='mb-2 block text-sm font-medium'>
@@ -61,13 +77,24 @@ export default function EditInvoiceForm({
 								type='number'
 								defaultValue={invoice && invoice.amount}
 								placeholder='Enter USD amount'
+								aria-describedby='amount-error'
 								className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
 							/>
 							<CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
 						</div>
 					</div>
+					{state.errors?.amount ? (
+						<div
+							id='amount-error'
+							aria-live='polite'
+							className='mt-2 text-sm text-red-500'
+						>
+							{state.errors.amount.map((error: string) => (
+								<p key={error}>{error}</p>
+							))}
+						</div>
+					) : null}
 				</div>
-
 				{/* Invoice Status */}
 				<fieldset>
 					<legend className='mb-2 block text-sm font-medium'>
@@ -82,6 +109,7 @@ export default function EditInvoiceForm({
 									type='radio'
 									value='pending'
 									defaultChecked={invoice && invoice.status === 'pending'}
+									aria-describedby='status-error'
 									className='h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600'
 								/>
 								<label
@@ -98,6 +126,7 @@ export default function EditInvoiceForm({
 									type='radio'
 									value='paid'
 									defaultChecked={invoice && invoice.status === 'paid'}
+									aria-describedby='status-error'
 									className='h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600'
 								/>
 								<label
@@ -110,6 +139,28 @@ export default function EditInvoiceForm({
 						</div>
 					</div>
 				</fieldset>
+				{state.errors?.status ? (
+					<div
+						id='status-error'
+						aria-live='polite'
+						className='mt-2 text-sm text-red-500'
+					>
+						{state.errors.status.map((error: string) => (
+							<p key={error}>{error}</p>
+						))}
+					</div>
+				) : null}{' '}
+				{state.errors?.customerId ||
+				state.errors?.amount ||
+				state.errors?.status ? (
+					<div
+						id='form-error'
+						aria-live='polite'
+						className='mt-2 text-sm text-red-500'
+					>
+						<p>{state.message}</p>
+					</div>
+				) : null}
 			</div>
 			<div className='mt-6 flex justify-end gap-4'>
 				<Link
@@ -118,7 +169,9 @@ export default function EditInvoiceForm({
 				>
 					Cancel
 				</Link>
-				<Button type='submit'>Edit Invoice</Button>
+				<Button type='submit' aria-describedby='form-error'>
+					Edit Invoice
+				</Button>
 			</div>
 		</form>
 	)
